9850f3789ec41de2cd2d90eeccf331e1a9ca49bb
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-01 11:47:55 +0200

    Identifying Realtime Potential

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -48,6 +48,20 @@ class Feed extends Component {
     });
   }
 
+  addPost = (post) => {
+    this.setState((prevState) => {
+      const updatedPosts = [...prevState.posts];
+      if (prevState.postPage === 1) {
+        updatedPosts.pop();
+        updatedPosts.unshift(post);
+      }
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1,
+      };
+    });
+  };
+
   loadPosts = (direction) => {
     if (direction) {
       this.setState({ postsLoading: true, posts: [] });
